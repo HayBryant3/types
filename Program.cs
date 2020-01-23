c9577ccf7df75529e15be5431750251404cb4d9a
HayBryant3 <59876349+HayBryant3@users.noreply.github.com>
2020-01-23 10:51:13 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Hailee";
+            int grade = 98;
+            char letterGrade = 'A';
+            double costOfTuition = 15000.00;
+
+            Console.WriteLine($"{firstName} is a student, {grade} is the grade she earned. Which is an {letterGrade} and it costs {costOfTuition} to attend her classes.");
+        }
+    }
+}
